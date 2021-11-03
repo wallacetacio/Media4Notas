2e97c651dc952e9ee862bb3079ad948b762dc812
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 10:03:10 -0300

    Adiciona resolução ao exercício Média 4 Notas ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Media4Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n1, n2, n3, n4, media;
+            string result;
+
+            Console.WriteLine("-- Média 4 Notas--\n");
+
+            Console.WriteLine("Digite as suas notas abaixo.");
+            
+            Console.Write("Nota 1: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 2: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 3: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Nota 4: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+
+            if(n1 < 0 || n1 > 10 
+            || n2 < 0 || n2 > 10 
+            || n3 < 0 || n3 > 10 
+            || n4 < 0 || n4 > 10 ){
+                
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Digite apenas notas entre 0 e 10.");
+                Console.ResetColor();
+            
+            }
+            else
+            {
+                
+                media = (n1 + n2 + n3 + n4) / 4;
+
+                if(media < 5.0){
+                    result = "Reprovado";
+                }
+                else if(media < 6.0){
+                    result = "Recuperação";
+                }
+                else{
+                    result = "Aprovado";
+                }
+
+                Console.Write($"Você ficou com a media {media:N2}. ");
+                Console.Write($"Resultado: ");
+                
+                if(result == "Reprovado"){
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else if(result == "Recuperação"){
+                     Console.ForegroundColor = ConsoleColor.DarkYellow;
+                }
+                else{
+                     Console.ForegroundColor = ConsoleColor.Green;
+                }
+                
+                Console.WriteLine($"{result}.");
+                Console.ResetColor();
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Media4Notas
-Exercício Média 4 Notas ETEC 2021
+## Exercício `Media4Notas` ETEC 2021
+
+Receba 4 notas, com somente uma casa decimal. Valide se todas estão entre 0.0 e 10.0. Se alguma delas não estiver, exiba "Digite somente notas entre 0 e 10.". Caso todas as notas sejam válidas, calcule a média aritmética das notas. Exiba uma mensagem no seguinte padrão: "Você ficou com média 7,5. Resultado: Aprovado".
+
+Resultados possíveis:
+
+- "Reprovado" para médias menores que 5.0
+- "Em recuperação" para médias entre 5.0 e 6.0
+- "Aprovado", para médias acima de 6.0
+
+Ex.:
+```
+-- Média --
+
+Digite as suas notas abaixo.
+Nota 1: 5,5
+Nota 2: -7,0
+Nota 3: 10,5
+Nota 4: 9,0
+
+Digite somente notas entre 0 e 10.
+```
+ou
+```
+-- Média --
+
+Digite as suas notas abaixo.
+Nota 1: 5,5
+Nota 2: 7,0
+Nota 3: 6,5
+Nota 4: 9,0
+
+Você ficou com média 7,0. Resultado: Aprovado
+```
+ou
+```
+-- Média --
+
+Digite as suas notas abaixo.
+Nota 1: 4,5
+Nota 2: 3,0
+Nota 3: 6,5
+Nota 4: 2,0
+
+Você ficou com média 4,0. Resultado: Reprovado
+```
+ou
+```
+-- Média --
+
+Digite as suas notas abaixo.
+Nota 1: 5,5
+Nota 2: 5,5
+Nota 3: 5,5
+Nota 4: 5,5
+
+Você ficou com média 5,5. Resultado: Em recuperação
+```
